a484d395d9b69e64224e9bd0691c496e1fb94ca8
Matt Schwartz <schwamat@gmail.com>
2020-10-25 11:23:13 -0500

    .

--- src/chronologer-web/src/lib/api/CommandletsApi.ts
+++ src/chronologer-web/src/lib/api/CommandletsApi.ts
@@ -1,14 +1,10 @@
 import { CurrencyModel } from "../data/CurrencyModel"
 import { constructUri } from './base';
 
-export interface BalanceCurrencyResponse {
-
-}
-
-export const BalanceCurrency = async (args: CurrencyModel): CurrencyModel => {
+export const BalanceCurrency = async (args: CurrencyModel): Promise<CurrencyModel> => {
     const uri: string = constructUri('currency/balance');
 
     const res = await fetch(uri, { method: 'POST', body: JSON.stringify(args) });
 
-    return (await res.json());
+    return res.json();
 }
